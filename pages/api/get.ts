4d84bab6e76fa2c0240289adbe97b3a849e31c0c
nawoo <now.and.nawoo@gmail.com>
2022-11-09 14:48:03 +0900

    add get API

---
+++ pages/api/get.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Redis } from "@upstash/redis/with-fetch";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const redis = Redis.fromEnv();
+  const key = "goerli-hourly";
+  const data = await redis.lrange(key, 0, 24 * 8); // 8日分
+
+  res.status(200).json(data);
+}
